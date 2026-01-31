4a9686e7b68e43ec4d0d1cace5c55cbb8b7a59ec
Madhu Srinivasan <madsrini@amd.com>
2026-01-31 01:16:16 -0600

    updated bashrc

---
+++ claude-code/env.sh
@@ -0,0 +1,9 @@
+source ~/.env
+export ANTHROPIC_API_KEY="dummy"
+export ANTHROPIC_BASE_URL="https://llm-api.amd.com/Anthropic"
+export ANTHROPIC_CUSTOM_HEADERS="Ocp-Apim-Subscription-Key: "$AMD_LLM_GATEWAY_KEY
+export ANTHROPIC_MODEL="Claude-Sonnet-4.5"
+export ANTHROPIC_DEFAULT_OPUS_MODEL="Claude-Opus-4.5"
+export ANTHROPIC_DEFAULT_SONNET_MODEL="Claude-Sonnet-4.5"
+export ANTHROPIC_DEFAULT_HAIKU_MODEL="Claude-Haiku-4.5"
+export CLAUDE_CODE_DISABLE_NONESSENTIAL_TRAFFIC=1
